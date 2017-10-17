38febe7089166116e9a2180ce0ce5adfc9ce0e33
Michael Filippov <michael@idisys.iae.nsk.su>
2017-10-17 11:54:55 +0700

    pixhawk/vision_pose: tf callback instead of polling; real timestamps

--- pixhawk/vision_pose.py
+++ pixhawk/vision_pose.py
@@ -1,20 +1,40 @@
 import rospy
-from camera_tracker import CameraTracker
-from geometry_msgs.msg import PoseStamped
 
+import tf
+from tf2_msgs.msg import TFMessage
+from std_msgs.msg import Header
+from geometry_msgs.msg import PoseStamped
 
 class VisionPose:
 
     def __init__(self):
-        self.camera_tracker = CameraTracker()
-        rospy.Timer(rospy.Duration(0.1), lambda event: self.__timer_callback())
+        self.marker_id = 'ar_marker_0'
+
+        self.transform_listener = tf.TransformListener()
+        rospy.Subscriber("tf", TFMessage,
+                         self.__tf_message_callback)
+
         self.vision_pose_estimate_pose_pub = rospy.Publisher('mavros/vision_pose/pose',
                                                              PoseStamped, queue_size=1)
 
-    def __timer_callback(self):
-        pose = self.camera_tracker.get_pose()
-        self.vision_pose_estimate_pose_pub.publish(pose)
-
+    def __tf_message_callback(self, msg):
+        for transform in msg.transforms:
+            if transform.child_frame_id == self.marker_id:
+                try:
+                    (p_list, q_list) = self.transform_listener.lookupTransform('/world', self.marker_id, transform.header.stamp)
+                    pose = PoseStamped()
+                    pose.header = Header()
+                    pose.header.stamp = transform.header.stamp
+                    pose.pose.position.x = p_list[0]
+                    pose.pose.position.y = p_list[1]
+                    pose.pose.position.z = p_list[2]
+                    pose.pose.orientation.x = q_list[0]
+                    pose.pose.orientation.y = q_list[1]
+                    pose.pose.orientation.z = q_list[2]
+                    pose.pose.orientation.w = q_list[3]
+                    self.vision_pose_estimate_pose_pub.publish(pose)
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException), e:
+                    rospy.logwarn(e)
 
 if __name__ == '__main__':
     rospy.init_node('vision_pose')
