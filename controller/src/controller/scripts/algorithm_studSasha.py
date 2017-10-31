b7eaf41e4b142149baebe73b9f324c333fc06296
Alex Maltsev <alexandr@idisys.iae.nsk.su>
2017-10-31 14:59:41 +0700

    algorithm_studSasha: delete unused variables

--- controller/src/controller/scripts/algorithm_studSasha.py
+++ controller/src/controller/scripts/algorithm_studSasha.py
@@ -16,18 +16,8 @@ class Algorithm:
         self.controller.set_control_loop(0.01, self.loop)
 
     def loop(self):
-        # 0.45; P: 0.2; D: 0
-        # thrust = 0.45
-        #    + 0.1 * (self.z_ref - self.controller.get_position().z) \
-        #   - 0.01 * (self.controller.get_linear_velocity().z)
         roll, pitch, yaw = 0, 0, 0
 
-        z_gr = self.controller.get_position().z
-        dz_gr = self.controller.get_linear_velocity().z
-
-        x_gr = self.controller.get_position().x
-        y_gr = self.controller.get_position().y
-        dx_gr = self.controller.get_linear_velocity().x
         k_z = 1.5
         alpha = k_z
         k_x = 0.3
@@ -35,14 +25,21 @@ class Algorithm:
         G = 9.81
         # T = 0.1
         # vz = (self.controller.get_position().z - self.z) / T
+        # self.z = self.controller.get_position().z
+
+        x_gr = self.controller.get_position().x
+        y_gr = self.controller.get_position().y
+        z_gr = self.controller.get_position().z
+
+        vx = self.controller.get_linear_velocity().x
         vy = self.controller.get_linear_velocity().y
         vz = self.controller.get_linear_velocity().z
-        self.z = self.controller.get_position().z
+
         Az = vz * (k_z + alpha) + k_z * alpha * (z_gr - self.z_ref) - G
-        Azz = Az
-        vx = self.controller.get_linear_velocity().x
         Ax = vx * (k_x + betta) + k_x * betta * (x_gr - self.x_ref)
         Ay = vy * (k_x + betta) + k_x * betta * (y_gr - self.y_ref)
+
+        Azz = Az
         Axx = Ax
         Ayy = Ay
 
@@ -52,8 +49,9 @@ class Algorithm:
         thrust = norm_coef * (M1 * sqrt(Azz * Azz + Axx * Axx))
         pitch = 10.0 * atan(Axx / Azz)
         roll = -10.0 * atan(Ayy / Azz)
-        rospy.loginfo('thrust(%.2f) z %.2f vz %.2f x %.2f dx_gr %.2f' % (thrust, z_gr, vz, x_gr, dx_gr))
-        self.log_model.write("%f %f %f %f %f %f %f\n" % (self.time(), thrust, z_gr, vz, x_gr, dx_gr, pitch))
+
+        rospy.loginfo('thrust(%.2f) z %.2f vz %.2f x %.2f vx %.2f' % (thrust, z_gr, vz, x_gr, vx))
+        self.log_model.write("%f %f %f %f %f %f %f %f %f %f %f %f %f %f\n" % (self.time(), thrust, z_gr, vz, pitch, x_gr, vx, roll, y_gr, vy, yaw, Axx, Ayy, Azz))
         self.log_model.flush()
 
         self.controller.set_control(roll, pitch, yaw, thrust)
