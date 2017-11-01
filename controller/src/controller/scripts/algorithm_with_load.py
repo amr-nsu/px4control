926705adb23994a90118bb3df2889a85cd4fb186
Alex Maltsev <alexandr@idisys.iae.nsk.su>
2017-11-01 17:43:00 +0700

    add algorithm_with_load

--- controller/src/controller/scripts/algorithm_with_load.py
+++ controller/src/controller/scripts/algorithm_with_load.py
@@ -1,40 +1,53 @@
 import rospy
 from math import *
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
 
 
 class Algorithm:
 
     def __init__(self, controller):
         self.controller = controller
-        self.z_ref = 0.8
+        self.z_ref = 1.8
         self.z = controller.get_position().z
         self.x_ref = 0.1
         self.y_ref = 0.01
+        self.gamma = 0.0
         self.x = controller.get_position().x
         self.time_start = self.controller.get_time()
         self.log_model = open('log/%s_regulation' % self.time_start, 'w')
         self.controller.set_control_loop(0.01, self.loop)
+        rospy.Subscriber('/odom', Odometry, self.__odometry_callback)
 
     def loop(self):
         roll, pitch, yaw = 0, 0, 0
 
         k_z = 1.5
         alpha = k_z
+        alpha2 = 1.0
+        k_gamma = 1.0
         k_x = 0.3
         betta = k_x
         G = 9.81
         # T = 0.1
         # vz = (self.controller.get_position().z - self.z) / T
         # self.z = self.controller.get_position().z
+        L = 1.0
+        M2 = 0.01
+        M1 = 0.42
 
-        #!!define gamma & vgamma & L & M2!! y??
-        x_gr = self.controller.get_position().x  + L*sin(gamma)
+        gamma = self.gamma
+        vgamma = 0 # FIXME
+        gamma = 0
+
+        # !!define gamma & vgamma & L & M2!! y??
+        x_gr = self.controller.get_position().x + L * sin(gamma)
         y_gr = self.controller.get_position().y
-        z_gr = self.controller.get_position().z - L*cos(gamma)
+        z_gr = self.controller.get_position().z - L * cos(gamma)
 
-        vx = self.controller.get_linear_velocity().x + vgamma*L*cos(gamma)
+        vx = self.controller.get_linear_velocity().x + vgamma * L * cos(gamma)
         vy = self.controller.get_linear_velocity().y
-        vz = self.controller.get_linear_velocity().z + vgamma*L*sin(gamma)    
+        vz = self.controller.get_linear_velocity().z + vgamma * L * sin(gamma)
 
         Az = M1*L*vgamma*vgamma*cos(gamma)/(M1+M2) + vz * (k_z + alpha) + k_z * alpha * (z_gr - self.z_ref) - G
         Ax = -M1*L*vgamma*vgamma*sin(gamma)/(M1+M2) + vx * (k_x + betta) + k_x * betta * (x_gr - self.x_ref)
@@ -44,7 +57,8 @@ class Algorithm:
         Axx = Ax  - 1.*cos(gamma)*M1*((alpha2+k_gamma)*vgamma+alpha2*k_gamma*gamma)/(M1+M2)
         Ayy = Ay
 
-        M1 = 0.42
+        print Ax, Ay, Az
+
         norm_coef = 1. * 0.45 / (M1 * G)
         # thrust = norm_coef * (M1 * sqrt(Azz * Azz))
         thrust = norm_coef * (M1 * sqrt(Azz * Azz + Axx * Axx))
@@ -59,3 +73,11 @@ class Algorithm:
 
     def time(self):
         return self.controller.get_time() - self.time_start
+
+    def __odometry_callback(self, message):
+        orientation = message.pose.pose.orientation
+        roll, pitch, yaw = euler_from_quaternion((orientation.x,
+                                                  orientation.y,
+                                                  orientation.z,
+                                                  orientation.w))
+        self.gamma = pitch
